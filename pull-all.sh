17c53c161ba2db4b31b4fefe006168a356812857
Glen Coakley <gcoakley@datarecognitioncorp.com>
2019-04-18 10:46:24 -0500

    Adds rest-request scripts and .gitignore.

--- pull-all.sh
+++ pull-all.sh
@@ -2,9 +2,10 @@
 
 curr_br=$(git branch | cut -c3-)
 BRANCH=${curr_br:=develop}
+// Run the required submodule bookkeeping.
 git submodule update --recursive --init --jobs 2
+# Checkout the HEAD of the branch.
 git submodule foreach --recursive "echo -e '\n'; git checkout $BRANCH"
-git submodule foreach --recursive "echo -e '\n'; git diff --submodule=log --no-color; git diff --shortstat origin/develop"
 
 echo -e '\n\nModules: (commit) (name) (fixed version)'
 git submodule status --recursive
