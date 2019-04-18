85bc8668176905b14375ee54915a39d830a98e00
Glen Coakley <gcoakley@datarecognitioncorp.com>
2019-04-18 12:06:17 -0500

    Automatically, sets this repo. to use the latest versions of eca-rtm-service and UI.

--- pull-all.sh
+++ pull-all.sh
@@ -6,6 +6,8 @@ BRANCH=${curr_br:=develop}
 git submodule update --recursive --init --jobs 2
 # Checkout the HEAD of the branch.
 git submodule foreach --recursive "echo -e '\n'; git checkout $BRANCH"
+# Set git to use the latest version of the rtm-service and rtm-ui projects. If neither changed this does nothing.
+git add service ui  &&  git commit -m 'Updates to latest versions of submodules'  &&  git push
 
 echo -e '\n\nModules: (commit) (name) (fixed version)'
 git submodule status --recursive
