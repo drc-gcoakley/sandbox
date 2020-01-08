c9ad89bca678ff84a2d1d0d2e19f66a5192e6293
Glen Coakley <gcoakley@datarecognitioncorp.com>
2020-01-07 18:36:09 -0600

    Adds a (less than useful) method to dump serverless config. object.

--- serverless-lib/drc-sls-functions.js
+++ serverless-lib/drc-sls-functions.js
@@ -1,17 +1,27 @@
 // const fs = require('fs');
 // const path = require('path');
 const AWS = require('aws-sdk');
-
+const Flatted = require('flatted/cjs');
 
 const awsSts = new AWS.STS();
-
 
 /**
  * Generate an AMI name composed of the date and the repo name
  * @return {String} AMI name
  */
 function todayAsIsoString() {
-  return 'TODAY'; //`${new Date().toISOString().replace(/-/g, '').substring(0, 8)}`;
+    return `${new Date().toISOString().replace(/-/g, '').substring(0, 8)}`;
+}
+
+/**
+ * Generate an AMI name composed of the date and the repo name
+ * @return {String} AMI name
+ */
+function dumpServerlessConfig(serverlessConfig) {
+    console.log("\n\nServerless configuration object:");
+    console.log(Flatted.stringify(serverlessConfig, null, 3));
+    console.log("\n\n");
+    return ''; // MUST return a string.
 }
 
 async function getAccountId() {
@@ -23,4 +33,5 @@ async function getAccountId() {
 module.exports = {
 	getAccountId,
 	todayAsIsoString,
+    dumpServerlessConfig,
 };
