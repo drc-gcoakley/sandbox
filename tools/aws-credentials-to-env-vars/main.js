60b281974bb240f009e446584b2452e31a5f6889
Glen Coakley <gcoakley@datarecognitioncorp.com>
2020-08-19 17:48:08 -0500

    Adds tool: aws-credentials-to-env-vars

---
+++ tools/aws-credentials-to-env-vars/main.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+let parser = require('@yaas/iniparser');
+
+let iniBody = fs.readFileSync(process.env.HOME + '/.aws/credentials', 'utf8');
+let iniJson = parser.parse(iniBody, '\n');
+let drcCred = iniJson['177429746880_AWS-SSO-Developers'];
+
+for (let k of Object.keys(drcCred)) {
+  console.log(`${k.toLocaleUpperCase()}=${drcCred[k]}`);
+}
